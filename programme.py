e9d6336b009395f1b2d799cd67637351e2efcb1e
shraddha9805 <shraddhamankar42@gmail.com>
2022-07-14 19:29:23 +0530

    first commit

---
+++ programme.py
@@ -0,0 +1,15 @@
+print()
+
+print("hello world python")
+
+print("shraddha manakr")
+
+print("Python 4")
+
+print("1", "2", "3", 4)
+
+print(1,2,3, sep=":")
+
+print(1,2,3, end="...\n")
+
+print(f"Value of 3*2 is {3*2}")
